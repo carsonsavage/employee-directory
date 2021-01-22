b1b3497792ec77c4ea6de40906fe64ed0fbaa6ab
Carson Savage <carsonesavage@gmail.com>
2021-01-22 17:01:36 -0600

    changed file structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import Main from './components/Main';
-import Wrapper from './components/Wrapper.js';
-import Header from './components/Header.js';
+import React, { useEffect, useState } from "react";
+import FilterInput from "./FilterInput";
+import Table from "./Table";
+import { getUsers } from "./API";
 import "./App.css";
 
 function App() {
+  const [initialUsers, updateAvailableUsers] = useState([]);
+  const [usersToRender, updateUsersToRender] = useState([]);
+
+  useEffect(() => {
+    getUsers().then(({ data: { results } }) => updateAvailableUsers(results));
+  }, []);
+
   return (
     <div className="App">
-    <Wrapper>
-      <Header />
-      <Main />
-    </Wrapper>
+      <h1>Employee Directory</h1>
+      <p>
+        To filter by first name, please begin your search in the user input below
+      </p>
+      <FilterInput users={initialUsers} updateUsers={updateUsersToRender} />
+      <Table users={usersToRender} />
     </div>
   );
 }
