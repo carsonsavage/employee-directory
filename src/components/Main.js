c7e1d768d6e9b9dbc45881822fbcbd3cda6f3edf
Carson Savage <carsonesavage@gmail.com>
2021-01-22 16:55:05 -0600

    added components

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import InfoMain from "./InfoMain.js";
+
+export default class Main extends Component {
+    render() {
+        return (
+        <>
+            <InfoMain />
+        </>
+    );
+}
+}
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 export default {
     getUsers: function() {
         return axios.get("https://randomuser.me/api/?results=200&nat=us")
